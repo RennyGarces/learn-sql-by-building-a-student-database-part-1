01ee99a40681a8b7151e11c0848e93971c4ae73f
Renny Garces Marriaga <rennygarces1983@gmail.com>
2024-06-04 08:27:43 +0000

    Save progress: 1060.1

--- insert_data.sh
+++ insert_data.sh
@@ -70,6 +70,6 @@ do
     # set to null
 
     # insert student
-
+INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id ,gpa) VALUES ('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
   fi
 done
