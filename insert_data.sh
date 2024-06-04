2834d5f14d3b090751f0c10200cb233213cafbda
Renny Garces Marriaga <rennygarces1983@gmail.com>
2024-06-04 08:30:23 +0000

    Save progress: 1070.1

--- insert_data.sh
+++ insert_data.sh
@@ -71,5 +71,11 @@ do
 
     # insert student
 INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id ,gpa) VALUES ('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
-  fi
+ 
+if [[ $INSERT_STUDENT_RESULT == "INSERT 0 1" ]]
+then
+ echo Inserted into students, $FIRST $LAST
+fi
+
+ fi
 done
